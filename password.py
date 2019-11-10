bf0fccfecad0f869fdebf4adb8708b57768b3317
Eric224 <naoki224@gmail.com>
2019-11-10 20:08:48 +0800

    password retry ending

--- password.py
+++ password.py
@@ -1,15 +1,15 @@
 password = 'a123456'
-numb = 2
-while numb >= 0 :
+numb = 3
+while numb > 0 :
+	numb = numb - 1
 	pwd = input('請輸入密碼： ')
 	if pwd == password :
 		print('密碼正確！')
 		break
 	else :
-		if numb != 0 :
-			print('密碼錯誤！ 你還有', numb, '次機會！')
-			numb = numb - 1
+		if numb > 0 :
+			print('密碼錯誤！')
+			print( '你還有', numb, '次機會！')
 		else :
 			print('密碼錯誤！')
-			break
 
